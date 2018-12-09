b955a571fbef5db2d64d77d71433e11d61caf3fc
Mattxu <pengcheng_xu@yeah.net>
2018-12-09 22:45:51 +0800

    add README.md file

---
+++ scanf.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+{
+    int score;
+    loop:
+    scanf("%d",&score);
+    if(score < 0 || score > 100)
+	{
+        printf("please input again:\n");
+        goto loop;
+    }
+
+    if((score >= 80) && (score <= 100))
+	{
+		printf("A\n");
+	}
+    else if((score >= 60) && (score <= 79))
+	{
+	    printf("B\n");
+	}
+    else if((score >= 40) && (score <= 59))
+	{
+        printf("C\n");		
+	}
+    else
+	{
+        printf("D\n");		
+	}
+
+    return 0;
+}
